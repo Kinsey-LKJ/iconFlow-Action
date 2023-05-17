6759d34e38535c54a83c8e3bf30bc942a5b116c5
Kinsey-LKJ <kinsey@lkj.design>
2023-05-17 12:07:26 +0800

    test: 💍 修改运行 Generate Icons and Build 的目录

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
 const { exec } = require('child_process');
-const path = require('path');
-const fs = require('fs-extra'); // Use fs-extra module
-
-const userRepoPath = path.resolve(process.env.GITHUB_WORKSPACE, '..');
-const actionRepoPath = path.resolve(process.env.GITHUB_WORKSPACE, 'action');
-
-// Copy entire 'src' directory from user's repository to action's repository
-fs.copySync(
-  path.join(userRepoPath, 'src'),
-  path.join(actionRepoPath, 'src')
-);
 
 exec(
-  `cd ${actionRepoPath} && npm run generate && npm run build:bundle`,
+  `npm run generate && npm run build:bundle`,
   (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
