f9c5cbf78ccb582e8a958acf52ada6e54575e87f
Kinsey-LKJ <kinsey@lkj.design>
2023-05-17 11:35:45 +0800

    test: 💍 将用户储存库的 src 目录复制到 action 目录

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const { exec } = require('child_process');
 const path = require('path');
+const fs = require('fs-extra'); // Use fs-extra module
 
-const actionRepoPath = path.resolve(__dirname);
+const userRepoPath = path.resolve(process.env.GITHUB_WORKSPACE, '..');
+const actionRepoPath = path.resolve(process.env.GITHUB_WORKSPACE, 'action');
+
+// Copy entire 'src' directory from user's repository to action's repository
+fs.copySync(
+  path.join(userRepoPath, 'src'),
+  path.join(actionRepoPath, 'src')
+);
 
 exec(
   `cd ${actionRepoPath} && npm run generate && npm run build:bundle`,
