c96924499fa034c5237f9bb92511a5806c3dfa56
Kinsey-LKJ <kinsey@lkj.design>
2023-05-17 15:20:30 +0800

    test: 💍 测试方案

--- main.js
+++ main.js
@@ -1,34 +1,24 @@
 const { exec } = require('child_process');
-const fs = require('fs-extra');
 const path = require('path');
+const fs = require('fs-extra'); // Use fs-extra module
 
-
-// Define the paths
-const actionRepoPath = process.env.GITHUB_WORKSPACE;
 const userRepoPath = path.resolve(process.env.GITHUB_WORKSPACE, '..');
+const actionRepoPath = path.resolve(process.env.GITHUB_WORKSPACE, 'action');
 
-console.log('Current working directory:', process.cwd());
+// Copy entire 'src' directory from user's repository to action's repository
+fs.copySync(
+  path.join(userRepoPath, 'src'),
+  path.join(actionRepoPath, 'src')
+);
 
-// Copy src directory from user repo to action repo
-fs.copySync(path.join(userRepoPath, 'src'), path.join(actionRepoPath, 'src'));
-
-// Change the working directory to the action repo
-process.chdir(actionRepoPath);
-
-console.log('Current working directory:', process.cwd());
-
-// Get the NPM auth token from the action inputs
-const npmAuthToken = process.env.INPUT_NPM_AUTH_TOKEN;
-
-// Create .npmrc file with auth token
-fs.writeFileSync('.npmrc', `//registry.npmjs.org/:_authToken=${npmAuthToken}`);
-
-// Install dependencies, generate icons, build bundle, and publish
-exec('npm install && npm run generate && npm run build:bundle && npm publish', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-});
+exec(
+  `cd ${actionRepoPath} && npm run generate && npm run build:bundle`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  },
+);
