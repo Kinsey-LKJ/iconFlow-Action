342f7eebb5db558ee0792aaea3c5f251210447d8
Kinsey-LKJ <kinsey@lkj.design>
2023-05-17 19:02:37 +0800

    feat: 🎸 实现读取用户的 package.json 文件中的版本号并修改 Action 中对应的值

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const { exec } = require('child_process');
 const path = require('path');
-const fs = require('fs-extra'); // Use fs-extra module
+const fs = require('fs');
 
 const userRepoPath = process.env.GITHUB_WORKSPACE
 const actionRepoPath = path.resolve(process.env.GITHUB_WORKSPACE, 'action');
 
-console.log(userRepoPath)
-console.log(actionRepoPath)
+const userPackagePath = path.join(__dirname, 'action', 'user-package.json');
+const userPackageContent = fs.readFileSync(userPackagePath, 'utf-8');
+const userPackageObj = JSON.parse(userPackageContent);
+const userVersion = userPackageObj.version;
+
+const packagePath = path.join(__dirname, 'package.json');
+let packageContent = fs.readFileSync(packagePath, 'utf-8');
+let packageObj = JSON.parse(packageContent);
+
+// 修改版本号
+packageObj.version = userVersion;
+
+// 将修改后的对象写回文件
+packageContent = JSON.stringify(packageObj, null, 2);
+fs.writeFileSync(packagePath, packageContent, 'utf-8');
 
 exec(
   `cd ${actionRepoPath} && npm run generate && npm run build:bundle`,
