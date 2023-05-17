a41f1abcf7c14f79744f576723e7eef8e6705767
Kinsey-LKJ <kinsey@lkj.design>
2023-05-17 16:00:53 +0800

    test: 💍 修改 main.js 里的命令

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const { exec } = require('child_process');
+const path = require('path');
+const fs = require('fs-extra'); // Use fs-extra module
+
+const userRepoPath = process.env.GITHUB_WORKSPACE
+const actionRepoPath = path.resolve(process.env.GITHUB_WORKSPACE, 'action');
+
+console.log(userRepoPath)
+console.log(actionRepoPath)
 
 exec(
-  `npm run generate && npm run build:bundle`,
+  `cd ${actionRepoPath} && npm run generate && npm run build:bundle`,
   (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
