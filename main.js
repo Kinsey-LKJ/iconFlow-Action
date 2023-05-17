e03dcd69030ffceba9cc47b3977188c0f91b76f6
Kinsey-LKJ <kinsey@lkj.design>
2023-05-17 15:55:34 +0800

    test: 💍 从 figma action 完成后的 src 目录里进行余下的操作

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
 const { exec } = require('child_process');
-const path = require('path');
-const fs = require('fs-extra'); // Use fs-extra module
-
-const userRepoPath = process.env.GITHUB_WORKSPACE
-const actionRepoPath = path.resolve(process.env.GITHUB_WORKSPACE, 'action');
-
-// Copy entire 'src' directory from user's repository to action's repository
-fs.copySync(
-  path.join(userRepoPath, 'src'),
-  path.join(actionRepoPath, 'src')
-);
-
-console.log(userRepoPath)
-console.log(actionRepoPath)
 
 exec(
-  `cd ${actionRepoPath} && npm run generate && npm run build:bundle`,
+  `npm run generate && npm run build:bundle`,
   (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
